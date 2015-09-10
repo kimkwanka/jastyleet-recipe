9335f2830d03f3e8e2dbddcd826b959a2b8e45e2
Kim Kwanka <kimkwanka@gmail.com>
2015-09-10 08:25:48 +0200

    Added autorename of main stylus file and changed readme accordingly

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ gulp-prettify.
 
 # Setup
 
-
 Assuming you already got a LAMP/MAMP/WAMP stack with wordpress installed:
 
 1.  Open the command line and navigate to where you downloaded jastyleet-toolchain
@@ -14,11 +13,12 @@ Assuming you already got a LAMP/MAMP/WAMP stack with wordpress installed:
     (Check your stack's manual for the localhost directory - this is the folder where all your web-facing files go)
   
 ```javascript
-    var htdocsPath = 'C:/xampp/htdocs/';  //this is where your lamp/mamp/wamp stack has it's localhost
-                                          //directory, depending on what stack you use it could
-                                          //be something like "C:/wamp/www/" too
-    var wpInstallName = 'wordpress4';     //the subfolder within your 'htdocs' where wordpress is installed
-    var themeName = 'jastyleet';          //this is the name of your theme folder
+    var htdocsPath = 'stack_local_host_folder';     //this is where your lamp/mamp/wamp stack has it's localhost
+                                                    //directory, depending on what stack you use it could
+                                                    //be something like "C:/wamp/www/"
+    var wpInstallName = 'wordpress';                //the subfolder within your 'htdocs' where wordpress is installed
+    var themeName = 'your_theme_name';              //this is the name of your theme folder
+    var mainStylusFile  = 'you_stylus_file';        //this is the name of your main stylus file
 ```
 ## Example:
 Let's say you installed wordpress into
@@ -30,20 +30,25 @@ C:/xampp/htdocs/wordpress
 and your theme is in
 
 ```
-C:/xampp/htdocs/wordpress/wp-content/themes/your_theme_name
+C:/xampp/htdocs/wordpress/wp-content/themes/my-awesome-theme
 ```
-
+and your main stylus file is called 
+```
+main.styl
+```
 you would change the 3 lines in gulpfile.js to:
 ```javascript
 var htdocsPath = 'C:/xampp/htdocs/';
 var wpInstallName = 'wordpress';
-var themeName = 'your_theme_name';
+var themeName = 'my-awesome-theme';
+var mainStylusFile  = 'main.styl';
 ```
+
 # Usage
 To use the toolchain simply type ```gulp serve``'.
 
 This will open a your offline wordpress site in a synced browser window/tab and silently watch all *.styl and *.jade files
-for changes and autocompile them.
+for changes and autocompile jade into corresponding *.php files and your main stylus file into 'style.css'.
     
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 /* global require, console */
 var gulp = require('gulp');
 
-var styluscompile = require('gulp-stylus');
-var jadecompile = require('gulp-jade-php');
-var rename = require("gulp-rename");
-var cached = require('gulp-cached');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync').create();
-var beautify = require('gulp-prettify');
-var koutoSwiss = require('kouto-swiss');
-var rupture      = require('rupture');
+var styluscompile   = require('gulp-stylus');
+var jadecompile     = require('gulp-jade-php');
+var rename          = require("gulp-rename");
+var cached          = require('gulp-cached');
+var plumber         = require('gulp-plumber');
+var browserSync     = require('browser-sync').create();
+var beautify        = require('gulp-prettify');
+var koutoSwiss      = require('kouto-swiss');
+var rupture         = require('rupture');
 
-//CHANGE THESE TO YOUR SPECIFIC FOLDER STRUCTURE!
-var htdocsPath = 'C:/xampp/htdocs/';
-var wpInstallName = 'wordpress4';
-var themeName = 'jastyleet';
+//CHANGE THESE TO YOUR MATCH YOUR SPECIFIC FOLDER STRUCTURE!
+var htdocsPath      = 'stack_local_host_folder';
+var wpInstallName   = 'wordpress';
+var themeName       = 'your_theme_name';
+var mainStylusFile  = 'you_stylus_file';
 
-var themesPath = htdocsPath +  wpInstallName + '/wp-content/themes/';
+var themesPath = htdocsPath + '/' +  wpInstallName + '/wp-content/themes/';
 
 var SRC = themesPath + themeName;
 var DIST = themesPath + themeName;
@@ -24,7 +25,7 @@ var DIST = themesPath + themeName;
 gulp.task('serve', function() {
     cached.caches = {};
     browserSync.init({
-         proxy: "localhost/" + wpInstallName
+        proxy: "localhost/" + wpInstallName
     });
 
     gulp.watch( SRC + '/**/*.styl', ['stylecompile']);
@@ -36,9 +37,13 @@ gulp.task('stylecompile', function() {
     var options = {
         use: [ koutoSwiss(), rupture() ]
     };
-    gulp.src(SRC + '/stylus/style.styl')
+    gulp.src(SRC + '/stylus/' + mainStylusFile)
         .pipe(plumber())
         .pipe(styluscompile(options))
+        .pipe(rename(function(file){
+            file.basename = 'style';
+            console.log('Compiled:',file.basename);
+        }))
         .pipe(gulp.dest(DIST))
         .pipe(browserSync.stream());
 });
@@ -49,7 +54,6 @@ gulp.task('jadecompile', function() {
         .pipe(plumber())
         .pipe(jadecompile())
         .pipe(rename(function(file){
-            //file.extname = ".php";
             console.log('Compiled:',file.basename);
         }))
         .pipe(beautify({indentSize: 2}))
